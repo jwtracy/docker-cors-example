2837dc729ec8e35bc43927b3bdfcd3e1b6cac5fd
John Tracy <jwtracy@verily.com>
2023-06-07 15:01:49 -0700

    Add Origin inspection Regex to support more than 1 domain

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -dit --rm -v ~/docker-cors/public-html/:/usr/local/apache2/htdocs --name c${1} -p 808${1}:80 httpd:2.4-custom
+docker run -dit --rm -e ALLOW_ORIGINS="localhost:8080|localhost:8081" -v ~/docker-cors/public-html/:/usr/local/apache2/htdocs --name c${1} -p 808${1}:80 httpd:2.4-custom
